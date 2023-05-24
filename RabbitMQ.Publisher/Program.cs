04a51830072bdc4e1ae9b93f4ae1dcd8de49aea9
KORUCU <yusuf.korucu@bilgeadam.com>
2023-05-24 11:20:19 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Connection
+ConnectionFactory connectionFactory = new ConnectionFactory();
+connectionFactory.Uri = new Uri("amqps://utmrfjnx:5EAFe_eJPnPfK04-bk1qfDp6YVP51MbT@chimpanzee.rmq.cloudamqp.com/utmrfjnx");
+
+//Connection active
+
+using IConnection connection = connectionFactory.CreateConnection();
+using IModel channnel = connection.CreateModel();
+
+//Crete Quee
+channnel.QueueDeclare(queue: "korucu-test-quee", exclusive: false);
+
+//Sending Quee Message RabbitMq Quee sended message byte type
+
+for (int i = 0; i < 25; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes("korucu test message"+i);
+
+    channnel.BasicPublish(exchange: "", routingKey: "korucu-test-quee", body: message);
+}
+
+
+Console.Read();
