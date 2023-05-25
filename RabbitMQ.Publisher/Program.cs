b3d2d0e4d410f1dd654a0ca0c6574b2914966327
KORUCU <yusuf.korucu@bilgeadam.com>
2023-05-25 17:13:45 +0300

    header exchange

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -10,19 +10,32 @@ connectionFactory.Uri = new Uri("amqps://utmrfjnx:5EAFe_eJPnPfK04-bk1qfDp6YVP51M
 using IConnection connection = connectionFactory.CreateConnection();
 using IModel channnel = connection.CreateModel();
 
-//Crete Quee
-channnel.QueueDeclare(queue: "korucu-test-quee", exclusive: false,durable:true);
 
-IBasicProperties properties = channnel.CreateBasicProperties();
-properties.Persistent= true;
+channnel.ExchangeDeclare(
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers
+    );
+
+
 
 //Sending Quee Message RabbitMq Quee sended message byte type
 
 for (int i = 0; i < 25; i++)
 {
-    byte[] message = Encoding.UTF8.GetBytes("korucu test message"+i);
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("korucu test message" + i);
 
-    channnel.BasicPublish(exchange: "", routingKey: "korucu-test-quee", body: message,basicProperties:properties);
+    Console.Write("Header value gir: ");
+    string value = Console.ReadLine();
+
+    var basicProperties = channnel.CreateBasicProperties();
+    basicProperties.Headers = new Dictionary<string, object>()
+    {
+        ["no"] = value
+    };
+    channnel.BasicPublish(exchange: "header-exchange-example", routingKey: string.Empty, body: message,
+        basicProperties: basicProperties);
+
 }
 
 
