d8f96097eaf4941fb9363a5794088c550c43a5d8
KORUCU <yusuf.korucu@bilgeadam.com>
2023-05-26 18:05:46 +0300

    message design

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
 //Connection
@@ -10,20 +11,124 @@ connectionFactory.Uri = new Uri("amqps://utmrfjnx:5EAFe_eJPnPfK04-bk1qfDp6YVP51M
 using IConnection connection = connectionFactory.CreateConnection();
 using IModel channnel = connection.CreateModel();
 
-//Crete Quee
-channnel.QueueDeclare(queue: "korucu-test-quee", exclusive: false,durable:true);
+#region P2P (Point to Point)
+
+//string queueName = "example-p2p-queue";
+
+//channnel.QueueDeclare(
+//    queue: queueName,
+//    durable: false,
+//    exclusive: false,
+//    autoDelete: false);
+
+//byte[] message = Encoding.UTF8.GetBytes("Merhaba Korucu");
+//channnel.BasicPublish(exchange: string.Empty,
+//    routingKey: queueName,
+//    body: message);
+
+#endregion
+
+#region Publish/Subscribe (pub/sub)
+
+//string exchangeName = "example-pub-sub-exchange";
+//channnel.ExchangeDeclare(
+//    exchange: exchangeName,
+//    type: ExchangeType.Fanout);
+
+//for (int i = 0; i < 100; i++)
+//{
+
+//    byte[] message = Encoding.UTF8.GetBytes($"Merhaba KORUCU {i}");
+
+//    channnel.BasicPublish(
+//        exchange: exchangeName,
+//        routingKey: string.Empty,
+//        body: message
+//        );
+//}
+
+#endregion
+
+#region Work Quee(İş kuyruğu)
+
+//string queueName = "example-work-queue";
+
+//channnel.QueueDeclare(
+//    queue: queueName,
+//    durable: false,
+//    exclusive: false,
+//    autoDelete: false);
+
+
+
+//for (int i = 0; i < 100; i++)
+//{
+//    await Task.Delay(185);
+//    byte[] message = Encoding.UTF8.GetBytes($"Merhaba KORUCU {i}");
+
+//    channnel.BasicPublish(
+//        exchange: string.Empty,
+//        routingKey: queueName,
+//        body: message
+//        );
+//}
+
+#endregion
+
+#region Request Response 
+string requestQueueName = "example-request-response-queue";
+
+channnel.QueueDeclare(queue: requestQueueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
+
+string replyQueueName = channnel.QueueDeclare().QueueName;
+
+string correlationId = Guid.NewGuid().ToString();
+
+#region Request Mesajı oluştur ve Gönder
 
 IBasicProperties properties = channnel.CreateBasicProperties();
-properties.Persistent= true;
+properties.CorrelationId = correlationId;
 
-//Sending Quee Message RabbitMq Quee sended message byte type
+properties.ReplyTo = replyQueueName;
 
-for (int i = 0; i < 25; i++)
+for (int i = 0; i < 10; i++)
 {
-    byte[] message = Encoding.UTF8.GetBytes("korucu test message"+i);
 
-    channnel.BasicPublish(exchange: "", routingKey: "korucu-test-quee", body: message,basicProperties:properties);
+    byte[] message = Encoding.UTF8.GetBytes($"KORUCU {i}");
+
+    channnel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: requestQueueName,
+        body: message,
+        basicProperties: properties);
 }
+
+#endregion
+
+#region Response Mesajı Dinleme
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channnel);
+channnel.BasicConsume(queue: replyQueueName,
+    consumer: consumer, autoAck: true);
+
+consumer.Received += Consumer_Received;
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+
+    if (e.BasicProperties.CorrelationId == correlationId)
+    {
+        Console.WriteLine($"Response {Encoding.UTF8.GetString(e.Body.Span)}");
+    }
+}
+
+#endregion
+
+
+#endregion
 
 
 Console.Read();
